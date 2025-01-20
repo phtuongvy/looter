3e5b963c3634b8d797f574a2597222824bab3930
phtuongvy <tuong-vy.pham-huynh@etu.univ-smb.fr>
2025-01-20 08:56:29 +0100

    Initial Commit

---
+++ looter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  looter
+//
+//  Created by Pham Huynh Tuong Vy on 20/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ looter/looterApp.swift
@@ -0,0 +1,17 @@
+//
+//  looterApp.swift
+//  looter
+//
+//  Created by Pham Huynh Tuong Vy on 20/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct looterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
